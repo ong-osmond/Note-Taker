2111ccbff0fc4dc2b3b820192ce27b4ff77fa795
ong_osmond <ong.osmond@yahoo.com>
2020-07-23 06:34:12 +0800

    Added handling for assets folder; added logging of saved Note.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { json } = require("express");
 
 // Sets up the Express App
 // =============================================================
@@ -16,19 +17,35 @@ const public = path.resolve(__dirname, "public");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Additional setup to allow Express to access the assets folder
+app.use("/assets", express.static(__dirname + "/assets"));
+
+// Initialise the Notes Array
+var notesArray = [];
+
 // Routes
 // =============================================================
 
 // API Route to return the notes in json format
 app.get("/api/notes", function(request, response) {
-    let data = JSON.parse(fs.readFileSync("./db/db.json"));
-    return response.json(data);
+    let existingNotes = JSON.parse(fs.readFileSync("./db/db.json"));
+    notesArray = existingNotes;
+    return response.json(existingNotes);
+});
+
+// API Route to save latest notes array
+app.post("/api/notes", function(request, response) {
+    var newNote = request.body;
+    notesArray.push(newNote);
+    console.log((notesArray));
+    response.end();
 });
 
 
-// Basic route that sends the user first to the AJAX Page
 
-app.get("*", function(request, response) {
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(request, response) {
+    //console.log(request.url);
     response.sendFile(path.join(public, "index.html"));
 });
 
@@ -37,6 +54,10 @@ app.get("/notes", function(request, response) {
     response.sendFile(path.join(public, "notes.html"));
 });
 
+// Route to home page if user does not enter an endpoint
+app.get("*", function(req, response) {
+    response.sendFile(path.join(__dirname, "index.html"))
+});
 
 
 // Starts the server to begin listening
