3efebf6b72fc1253a86128fcbd25075188058405
ong_osmond <ong.osmond@yahoo.com>
2020-07-24 00:05:35 +0800

    Added saving of Note IDs and delete note function.

--- server.js
+++ server.js
@@ -25,28 +25,53 @@ app.use("/db", express.static(__dirname + "/db"));
 // Initialise the Notes Array
 var notesArray = [];
 
+// Function to update the db.json file
+function updateNotes(notesArray) {
+    notesArray = JSON.stringify(notesArray);
+    fs.writeFileSync("./db/db.json", notesArray);
+}
+
 // Routes
 // =============================================================
 
 // API Route to return the notes in json format
 app.get("/api/notes", function(request, response) {
-    let existingNotes = JSON.parse(fs.readFileSync("./db/db.json"));
-    notesArray = existingNotes;
-    return response.json(existingNotes);
+    notesArray = JSON.parse(fs.readFileSync("./db/db.json"));
+    return response.json(notesArray);
 });
 
 // API Route to save latest notes array
 app.post("/api/notes", function(request, response) {
-    var newNote = request.body;
+    notesArray = JSON.parse(fs.readFileSync("./db/db.json"));
+    let newNote = request.body;
+    //Add an id to the note
+    if (typeof notesArray !== 'undefined' && notesArray.length > 0) {
+        let lastNoteId = notesArray[notesArray.length - 1].id
+        newNote.id = lastNoteId + 1;
+    } else {
+        newNote.id = 1;
+    }
     notesArray.push(newNote);
-    notesArray = JSON.stringify(notesArray);
-    fs.writeFileSync("./db/db.json", notesArray);
+    updateNotes(notesArray);
+    response.end();
+});
+
+// API Route to delete a note
+app.delete("/api/notes/:id", function(request, response) {
+    notesArray = JSON.parse(fs.readFileSync("./db/db.json"));
+    let noteToDeleteId = request.params.id;
+    console.log(noteToDeleteId);
+    for (var i = 0; i < notesArray.length; i++) {
+        if (noteToDeleteId == notesArray[i].id) {
+            notesArray.splice(i, 1);
+        }
+    }
+    updateNotes(notesArray);
     response.end();
 });
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(request, response) {
-    //console.log(request.url);
     response.sendFile(path.join(public, "index.html"));
 });
 
@@ -56,7 +81,7 @@ app.get("/notes", function(request, response) {
 });
 
 // Route to home page if user does not enter an endpoint
-app.get("*", function(req, response) {
+app.get("*", function(request, response) {
     response.sendFile(path.join(__dirname, "index.html"))
 });
 
